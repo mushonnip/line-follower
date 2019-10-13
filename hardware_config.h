6b1d24118f0bd2203863f39eb0adcbdfa720907c
Abu Mushonnip <mushonnipabu@gmail.com>
2019-10-13 21:10:37 +0700

    Create hardware_config.h

---
+++ hardware_config.h
@@ -0,0 +1,53 @@
+// config for linebot pro/ultimate 
+//#define pin_button_UPL 16
+//#define pin_button_DOWNL 20
+//#define pin_button_UPR 17
+//#define pin_button_DOWNR 21
+//#define pin_button_START 18
+//#define pin_button_MENU 19
+//
+//#define pin_LCDRS 0
+//#define pin_LCDRW 1
+//#define pin_LCDE 2
+//#define pin_LCDD4 4
+//#define pin_LCDD5 5
+//#define pin_LCDD6 6
+//#define pin_LCDD7 7
+//#define pin_LCDLED 3
+//
+//#define pin_MOTOR_DIRL 11
+//#define pin_MOTOR_PWML 12
+//#define pin_MOTOR_DIRR 14
+//#define pin_MOTOR_PWMR 13
+//
+//#define pin_ENABLE_SENSORL 23
+//#define pin_ENABLE_SENSORR 22
+//
+//const int pin_ADC_SENSOR[14] = {A7, A6, A5, A4, A3, A2, A1, A1, A2, A3, A4, A5, A6, A7};
+
+// config for linebot ultimate SMD
+#define pin_button_UPL 2
+#define pin_button_DOWNL 6
+#define pin_button_UPR 3
+#define pin_button_DOWNR 7
+#define pin_button_START 4
+#define pin_button_MENU 5
+
+#define pin_LCDRS 23
+#define pin_LCDRW 22
+#define pin_LCDE 21
+#define pin_LCDD4 20
+#define pin_LCDD5 19
+#define pin_LCDD6 18
+#define pin_LCDD7 17
+#define pin_LCDLED 16
+
+#define pin_MOTOR_DIRL 11
+#define pin_MOTOR_PWML 12
+#define pin_MOTOR_DIRR 14
+#define pin_MOTOR_PWMR 13
+
+#define pin_ENABLE_SENSORL 0
+#define pin_ENABLE_SENSORR 1
+
+const int pin_ADC_SENSOR[14] = {A0, A1, A2, A3, A4, A5, A6, A6, A5, A4, A3, A2, A1, A0};
